33fe33d715819ced0d33540bed112400abe0b631
Vlad <rubilnik56@gmail.com>
2022-10-18 16:59:54 +0300

    chuck

--- src/main/java/GodAdministrationApp.java
+++
@@ -1,6 +0,0 @@
-public class GodAdministrationApp {
-    public static void main(String[] args) {
-        System.out.println("heljfchg" +
-                "lo world");
-    }
-}
---
+++ src/main/java/Opinion.java
@@ -0,0 +1,3 @@
+public enum Opinion {
+    YES, NO
+}
